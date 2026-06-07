5c23cdf994e23cf79be954ef0b36a79184908732
manwithavan <caovinhthai13042006@gmail.com>
2026-06-07 10:25:34 +0300

    Update circuitanalyzer(1).py

--- circuitanalyzer(1).py
+++ circuitanalyzer(1).py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1UDRbv5xSMXYB7NRo-ucuFt6WvavidFqo
 """
 
-═
+
 !pip install qiskit qiskit-aer matplotlib networkx seaborn scipy -q
 print("✅ Dependencies installed")
 
